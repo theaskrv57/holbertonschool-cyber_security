66e2d6c1f77a32471353dcfd2fd79d19c05064f9
theaskrv57 <askerovsahil5757@gmail.com>
2025-12-23 11:02:20 +0000

    cyber

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk 'BEGIN{ORS="";f=1}/^(Registrant|Admin|Tech) /{s=$1}/Name:|Organization:|Street:|City:|State\/Province:|Postal Code:|Country:|Phone Ext:|Phone:|Fax Ext:|Fax:|Email:/{if(!f)printf "\n";f=0;sub(/.*: */,"",$0);k=$1;if(k=="Street")$0=$0" ";printf "%s %s,%s",s,k,$0}'
+whois "$1" | awk 'BEGIN{ORS="";f=1}/^(Registrant|Admin|Tech) /{s=$1}/Name:|Organization:|Street:|City:|State\/Province:|Postal Code:|Country:|Phone Ext:|Phone:|Fax Ext:|Fax:|Email:/{if(!f)printf "\n";f=0;sub(/.*: */,"",$0);k=$1;if(k=="Street")$0=$0" ";printf "%s %s,%s",s,k,$0}' > "$1.csv"
