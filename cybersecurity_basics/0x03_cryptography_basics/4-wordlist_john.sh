f3178d74ba878435fec22227fc7c7a12da95228e
theaskrv57 <askerovsahil5757@gmail.com>
2026-02-12 06:22:08 -0500

    cyber

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | cut -d: -f2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1 && john --show "$1" | cut -d: -f2 | grep . > 4-password.txt
