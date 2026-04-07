578884141e155270167e463353464f6c6652e928
theaskrv57 <askerovsahil5757@gmail.com>
2026-04-07 08:09:57 +0000

    nmap

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap --scanflags -oN FIN,SYN,RST,PSH,ACK,URG -p "$2" "$1" > custom_scan.txt 2>&1
+sudo nmap --scanflags -oN FIN,SYN,RST,PSH,ACK,URG -p "$2" "$1" > custom_scan.txt 2>&1 >/dev/null
